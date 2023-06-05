4046e0adbf363742d4b35b9ad4ee60dd06648370
Rahul Bhope <rahulatulbhope@yahoo.co.in>
2023-06-05 15:13:46 -0700

    changed it

---
+++ fetch_api.py
@@ -0,0 +1,45 @@
+# write an API to connect to the database, and change the text to be in a usable format 
+# [a usable format is the text form for abstract and the rest of the document as a long text separated by \n]
+import arxiv
+import re
+
+import pandas as pd
+import numpy as np
+
+def get_clean_text(paper_id):
+    
+    search = arxiv.Search(id_list=["2111.11418v1"])
+    papers = next(search.results())
+    print(papers.title)
+
+
+    if len(papers.title) > 0:
+        paper = papers
+        pdf_url = paper.pdf_url
+
+        # Extract text from the PDF
+        import io
+        import requests
+        import PyPDF2
+
+        response = requests.get(pdf_url)
+        pdf_file = io.BytesIO(response.content)
+
+        # Create a PDF reader object
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
+
+        # Extract text from each page
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+
+        cleaned_text = text[:text.index('Abstract')] + text[text.index('Introduction'):]
+
+        print(cleaned_text)#print(text)
+        
+        return cleaned_text, paper.summary
+
+    else:
+        print("Paper not found.")
+        return '',''
--- test_api.py
+++ test_api.py
@@ -3,15 +3,19 @@
 
 import pandas as pd
 import numpy as np
-import api
-
+import fetch_api
+import main_script
 df = pd.read_csv('arxiv_papers.csv', delimiter=',', encoding = "utf8")
 
 df_1 = df.head()
 
-for row,index in df_1.iterrows():
-  paper_id = row['url'][row['url'].rindex('/')+1:]
-  main_text, abstract = api.get_clean_text(paper_id)
-  print(paper_id)
+for index,row in df_1.iterrows():
+    paper_id = row['url'][row['url'].rindex('/')+1:]
+    main_text, abstract = fetch_api.get_clean_text(paper_id)
+    print(paper_id)
+    
+    print(main_script.link_abstract_sentences_to_paragraphs(abstract,main_text))
+    
+    
   
 
